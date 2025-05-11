3755c32d2f3cf9f5c2161738e17c3c4ace6acea5
Chanelle Noble <chanelle.noble9@gmail.com>
2025-05-11 15:01:37 +1000

    Update dsmtx_hrf.m

--- dsmtx_hrf.m
+++ dsmtx_hrf.m
@@ -3,6 +3,9 @@
 %tr is the number of repeats
 %% NB:
 % Requires  @(#)spm_hrf.m	2.8 Karl Friston 02/07/31 in order to run.
+% SPM2 - Statistical Parametric Mapping. Wellcome Centre for Human Neuroimaging, UCL. Available at: https://www.fil.ion.ucl.ac.uk/spm/
+%
+% Citation: Friston, K.J., et al. (2003). Statistical parametric mapping: The analysis of functional brain images. Academic Press.
  
 
 function dsmtx = dsmtx_hrf(onsets,tr)
