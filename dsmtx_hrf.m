855c9c5b702b348f5a6c6257f14963a7986584e7
Chanelle Noble <chanelle.noble9@gmail.com>
2025-05-11 14:59:19 +1000

    Update dsmtx_hrf.m

--- dsmtx_hrf.m
+++ dsmtx_hrf.m
@@ -1,3 +1,10 @@
+%onset defines the start time of the event in secs
+%duration defines the duration for the event in secs
+%tr is the number of repeats
+%% NB:
+% Requires  @(#)spm_hrf.m	2.8 Karl Friston 02/07/31 in order to run.
+ 
+
 function dsmtx = dsmtx_hrf(onsets,tr)
 
     hrf = spm_hrf(tr);
